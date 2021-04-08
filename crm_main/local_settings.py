686ec5a662306f5ec12b26d7eb8bf33d6ab59357
Mandaw17 <diopmandaw17@gmail.com>
2021-04-08 18:32:15 +0200

    fixing database issues

--- crm_main/local_settings.py
+++ crm_main/local_settings.py
@@ -9,21 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'django_crm',
-    #    'USER': 'root',
-    #    'PASSWORD': 'passer',
-    #    'HOST': 'localhost',
-    #    'PORT': '3306',
-    #},
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd93ove0iknaav4',
-        'USER': 'kixsocstmvohye',
-        'PASSWORD': '6c9d339010b93f6873ae55f66c1ee812d1c00984130816a71df24e15dec7de4f',
-        'HOST': 'ec2-107-22-245-82.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_crm',
+        'USER': 'root',
+        'PASSWORD': 'passer',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
