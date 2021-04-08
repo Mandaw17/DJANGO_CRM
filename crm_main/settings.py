a37ce9b78c9d2fca2f4022d5b6476c250298b472
Mandaw17 <diopmandaw17@gmail.com>
2021-04-08 15:55:03 +0200

    adding requiresments for static files

--- crm_main/settings.py
+++ crm_main/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
